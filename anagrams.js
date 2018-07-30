52304b25b3024de4167d24765a17bba347f430b6
harleyjj <harleyyesm@gmail.com>
2018-07-30 13:12:14 -0700

    start anagram grouping

---
+++ anagrams.js
@@ -0,0 +1,14 @@
+'use strict';
+const HashMap = require('./HashMap');
+
+function anagramGrouping(arr) {
+  //for each string in the array, build a count hashmap
+  //where a count hashmap is the same, add it to a group, otherwise start another group
+}
+
+const hash1 = new HashMap();
+const hash2 = new HashMap();
+hash1.set('test', 1);
+hash2.set('test', 1);
+
+console.log(hash1 == hash2);
